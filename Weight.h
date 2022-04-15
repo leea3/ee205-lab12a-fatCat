31cd368f212b121b73f90500faf0d88de07df6c2
leea3 <leea3@hawaii.edu>
2022-04-15 11:41:11 -1000

    stubbed out weight.cpp/.h and cmake file

---
+++ Weight.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Arthur Lee <leea3@hawaii.edu>
+/// @date   15_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+
+class Weight {
+
+};
