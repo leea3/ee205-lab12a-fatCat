c211934af00728d838af88c0bcafa1171118031a
leea3 <leea3@hawaii.edu>
2022-04-16 22:01:41 -1000

    added comments

--- main.cpp
+++ main.cpp
@@ -16,24 +16,33 @@ using namespace std;
 int main(){
     cout << "Hello World!" << endl;
 
+    //testing constructor
     Weight test1 = Weight(1.0, Weight::KILO, 40.0);
     test1.dump();
 
+    //testing negative values
     //Weight test2 = Weight(-5.5, Weight::KILO, 40.0);
+
+    //testing another constructor
     Weight test3 = Weight(5.0);
     test3.dump();
 
+    //testing weight greater than maxweight (should fail)
     //Weight test4 = Weight(500, Weight::KILO, 50);
 
+    //setting unknown weight
     Weight test5 = Weight(Weight::UNKNOWN_WEIGHT, Weight::KILO, 40.0);
     test5.dump();
 
+    //changing maxweight to a different value (should fail)
     Weight test6 = Weight(5.0, Weight::KILO, 55);
     //test6.setMaxWeight( 52 );
 
+    //changing weight from known to unknown (should fail)
     Weight test7 = Weight(5.0, Weight::KILO, 55);
     //test7.setWeight( Weight::UNKNOWN_WEIGHT );
 
+    //testing += operator
     Weight test8 = Weight(5.0, Weight::POUND, 55);
     test8.dump();
     test8 += 10;
